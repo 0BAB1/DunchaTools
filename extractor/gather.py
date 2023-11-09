5d709f1b0afcf66cbb5475000df2221d75f52ad6
hugo B <hb.hugobabin@gmail.com>
2023-11-09 17:31:22 +0100

    handle corrupt file error

--- extractor/gather.py
+++ extractor/gather.py
@@ -16,23 +16,28 @@ def getData(file_path : list, text_mode = False) -> list:
         #lire chaque page
         for page in pdf.pages:
             
-            if not text_mode:
-                #extraire les tebleaux de la page
-                tables = page.find_tables()
+            try : #sometimes, files are courrupted, so we try and raise an exception taht can be handled in the extraction thread.
+                if not text_mode:
+                    #extraire les tebleaux de la page
+                    tables = page.find_tables()
 
-                for table in tables:
-                    table = table.extract()
-                    #lire chaque ligne (de chaque page) (de chque tableau)
-                    for line in table:
+                    for table in tables:
+                        table = table.extract()
+                        #lire chaque ligne (de chaque page) (de chque tableau)
+                        for line in table:
+                            data.append(line)
+                        
+                elif text_mode:
+                    #extraire le texte de la page
+                    text = page.extract_text()
+                    #lire chaque ligne (de chaque page)
+                    for line in text.split("\n"):
+                        line = sort.treat_line(line)
                         data.append(line)
-                    
-            elif text_mode:
-                #extraire le texte de la page
-                text = page.extract_text()
-                #lire chaque ligne (de chaque page)
-                for line in text.split("\n"):
-                    line = sort.treat_line(line)
-                    data.append(line)
+                        
+            except Exception as e:
+                #we raise an error if file is corrupted, file_path.split("/")[0] is the nae of the file at the end of the path
+                raise Exception("Erreur FICHIER CORROMPU : " + file_path.split("/")[-1] + "\n" + "Code erreur : " + str(e))
                     
         print(file_path.split("/")[-1] + " was successfully processed ! Processing next file ...")
 
